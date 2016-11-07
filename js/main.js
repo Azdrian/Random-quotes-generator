81f1809cd33df8fe74dbe41a41373dbed26a16a2
U'el Azdrian Small <azdrian18@hotmail.com>
2016-11-06 22:20:48 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
+var $quoteUrl = "https://crossorigin.me/http://api.forismatic.com/api/1.0/?    method=getQuote&lang=en&format=jsonp&jsonp=?",
+  $quote, $author,
+  $getQuote = function() {
+    $.ajax({
+      url: $quoteUrl,
+      type: 'GET',
+      dataType: 'jsonp',
+      success: function(data) {
 
+        if (data.quoteAuthor === "") {
+          $author = $("#author").text(data.quoteAuthor = "Unknown");
+        }
+        $tweetUrl = "https://twitter.com/intent/tweet?text='" + data.quoteText + "' " + data.quoteAuthor;
+
+        $(".twitter-share-button").attr("href", $tweetUrl);
+
+        $quote = $("#quote").text(data.quoteText);
+        $author = $("#author").text(data.quoteAuthor);
+
+        $('#random-quote').append($quote).append($author);
+
+      }
+
+    });
+  };
+
+$(document).ready(function() {
+  $.getJSON($quoteUrl, $getQuote, "jsonp");
+
+  $("#random-button").on("click", function() {
+    $.getJSON($quoteUrl, $getQuote, "jsonp");
+  });
+
+  $('#quote,#author').addClass("animated zoomIn");
+  /*
+  $( "#quote".before( '" ');
+  $( "#quote".after( ' "');
+  */
+
+});
