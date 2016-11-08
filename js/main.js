0e611fcf4553ae951e4c7fbd8c751afb7abf7133
U'el Azdrian Small <azdrian18@hotmail.com>
2016-11-07 21:19:29 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
-var $quoteUrl = "https://crossorigin.me/http://api.forismatic.com/api/1.0/?    method=getQuote&lang=en&format=jsonp&jsonp=?",
-  $quote, $author,
-  $getQuote = function() {
+/* 
+  *Author: U'el Azdrian small
+  *Date: Copyright, November 3rd 2016
+  *File Name: Main.css
+  *Website: https://azdrian.github.io/Random-quotes-machine/
+
+*/
+$(document).ready(function() {
+// Declare viariables
+var $quoteUrl = "https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?",
+    $quote,
+    $author;
+
+// make a request to the foristmatic API for random quotes and display it on the page
+var $getQuote = function() {
     $.ajax({
       url: $quoteUrl,
       type: 'GET',
@@ -24,17 +36,16 @@ var $quoteUrl = "https://crossorigin.me/http://api.forismatic.com/api/1.0/?    m
     });
   };
 
-$(document).ready(function() {
+   //Request  quote when page is loaded
   $.getJSON($quoteUrl, $getQuote, "jsonp");
-
+  
+  //Get a random quote when button is click
   $("#random-button").on("click", function() {
     $.getJSON($quoteUrl, $getQuote, "jsonp");
   });
 
+  //Add class for zoom in effect when a quote is requested upon clicking the random button
   $('#quote,#author').addClass("animated zoomIn");
-  /*
-  $( "#quote".before( '" ');
-  $( "#quote".after( ' "');
-  */
+ 
 
 });
